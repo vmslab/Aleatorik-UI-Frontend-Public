bc1e4918a53979c4f407b44bdf400637df01655a
Hawon Kim <hawonkim@vms-solutions.com>
2023-04-20 18:41:24 +0900

    팝업에서 뒷배경 삭제

--- packages/standard/src/utils/dialog.ts
+++ packages/standard/src/utils/dialog.ts
@@ -1,12 +1,12 @@
-import { confirm } from "devextreme/ui/dialog";
-import { alert } from "devextreme/ui/dialog";
-import notify from "devextreme/ui/notify";
-import { Popup } from "@grapecity/wijmo.input";
-import { t } from "i18next";
+import { confirm } from 'devextreme/ui/dialog';
+import { alert } from 'devextreme/ui/dialog';
+import notify from 'devextreme/ui/notify';
+import { Popup } from '@grapecity/wijmo.input';
+import { t } from 'i18next';
 
 export interface ISnackBarParams {
   message: string;
-  type: "error" | "warning" | "success" | "info";
+  type: 'error' | 'warning' | 'success' | 'info';
 }
 
 export const getDialogText = (message: string, type: string) => {
@@ -17,13 +17,13 @@ export const getDialogText = (message: string, type: string) => {
 };
 
 export const showAlert = async (params: { message: string; type?: string; title?: string }) => {
-  const { message, type = "warning", title = "Warning" } = params;
+  const { message, type = 'warning', title = 'Warning' } = params;
   return await alert(getDialogText(message, type), title);
 };
 
 export const showConfirm = async (params: { message: string; type?: string; title?: string }) => {
-  const { message, type = "warning", title = "Confirm" } = params;
-  return await confirm(getDialogText(message, type), "Confirm");
+  const { message, type = 'warning', title = 'Confirm' } = params;
+  return await confirm(getDialogText(message, type), 'Confirm');
 };
 
 /**
@@ -32,45 +32,45 @@ export const showConfirm = async (params: { message: string; type?: string; titl
  * @returns
  */
 export const showDialog = async (params: {
-  type: "error" | "warning" | "success" | "info";
+  type: 'error' | 'warning' | 'success' | 'info';
   message: string;
-  action: "confirm" | "alert";
+  action: 'confirm' | 'alert';
 }) => {
   return new Promise((resolve, reject) => {
-    const { message, type = "warning", action = "confirm" } = params;
+    const { message, type = 'warning', action = 'confirm' } = params;
 
-    const container = document.createElement("div");
-    container.className = "moz-dialog";
+    const container = document.createElement('div');
+    container.className = 'moz-dialog';
 
-    const header = document.createElement("div");
-    header.className = "wj-dialog-header moz-dialog-header";
+    const header = document.createElement('div');
+    header.className = 'wj-dialog-header moz-dialog-header';
     header.textContent = getDialogTitle(type);
     // TODO: icon 변경시 textContent 말고 자식으로 넣어야 함
 
-    const body = document.createElement("div");
-    body.className = "wj-dialog-body moz-dialog-body";
+    const body = document.createElement('div');
+    body.className = 'wj-dialog-body moz-dialog-body';
     body.textContent = message;
 
-    const actions = document.createElement("div");
-    actions.className = "wj-dialog-footer moz-dialog-footer";
+    const actions = document.createElement('div');
+    actions.className = 'wj-dialog-footer moz-dialog-footer';
 
-    if (action == "confirm") {
+    if (action == 'confirm') {
       // 버튼 두개
-      const yes = document.createElement("button");
-      yes.className = "moz-button moz-default-button wj-hide-yes";
-      yes.textContent = t("Yes");
+      const yes = document.createElement('button');
+      yes.className = 'moz-button moz-default-button wj-hide-yes';
+      yes.textContent = t('Yes');
 
-      const no = document.createElement("button");
-      no.className = "moz-button moz-text-button wj-hide-no";
-      no.textContent = t("No");
+      const no = document.createElement('button');
+      no.className = 'moz-button moz-text-button wj-hide-no';
+      no.textContent = t('No');
 
       actions.appendChild(yes);
       actions.appendChild(no);
-    } else if (action == "alert") {
+    } else if (action == 'alert') {
       // 버튼 하나만
-      const yes = document.createElement("button");
-      yes.className = "moz-button moz-default-button wj-hide-yes";
-      yes.textContent = t("Yes");
+      const yes = document.createElement('button');
+      yes.className = 'moz-button moz-default-button wj-hide-yes';
+      yes.textContent = t('Yes');
     }
 
     // append childrens at container
@@ -80,27 +80,29 @@ export const showDialog = async (params: {
 
     const popup = new Popup(container, {
       isDraggable: true,
+      modal: true,
+      hideTrigger: 0
     });
     popup.show(true, (sender: any) => {
-      resolve(sender.dialogResult == "wj-hide-yes");
+      resolve(sender.dialogResult == 'wj-hide-yes');
     });
   });
 };
 
 const getDialogTitle = (type: string) => {
   switch (type) {
-    case "error":
-      return "err";
+    case 'error':
+      return 'err';
 
-    case "warning":
-      return "warn";
+    case 'warning':
+      return 'warn';
 
-    case "success":
-      return "succ";
+    case 'success':
+      return 'succ';
 
-    case "info":
+    case 'info':
     default:
-      return "info";
+      return 'info';
   }
 };
 
@@ -109,23 +111,23 @@ export const showSnackBar = (params: ISnackBarParams) => {
     message: params.message,
     type: params.type,
     displayTime: 2000,
-    width: "auto",
+    width: 'auto',
     minWidth: 150,
     animation: {
       show: {
-        type: "fade",
+        type: 'fade',
         duration: 400,
         from: 0,
-        to: 1,
+        to: 1
       },
-      hide: { type: "fade", duration: 40, to: 0 },
-    },
+      hide: { type: 'fade', duration: 40, to: 0 }
+    }
   });
 };
 
 export const showMessage = (message: any, result: boolean) => {
   showSnackBar({
     message: `${message}!`,
-    type: result ? "success" : "error",
+    type: result ? 'success' : 'error'
   });
 };
