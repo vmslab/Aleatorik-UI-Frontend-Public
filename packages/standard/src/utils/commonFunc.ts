7e34f7d17316c6f9d6e8aff189b2e0fa0bf69962
Hawon Kim <hawonkim@vms-solutions.com>
2023-04-20 14:49:00 +0900

    그리드 키 컬럼 수정 불가처리하는 유틸 함수 추가

--- packages/standard/src/utils/commonFunc.ts
+++ packages/standard/src/utils/commonFunc.ts
@@ -17,3 +17,24 @@ export const pick = (obj: any, keys: string[]) => {
   // Return new object
   return picked;
 };
+
+/**
+ * Wijmo Grid 이벤트에서 실제 데이터를 가진 셀인지 확인하는 함수
+ * @param grid FlexGrid
+ * @param e Event
+ * @returns
+ */
+export const isDataCell = (grid: any, e: any) => {
+  return grid.cells === e.panel;
+};
+
+/**
+ * 그리드 키 컬럼에 대해 새로 추가된 row만 수정 가능하고 기존 row는 수정할 수 없게 하는 함수
+ * @param grid
+ * @param e
+ */
+export const disableKeyColumnEdit = (grid: any, e: any, gridKeys: string[]) => {
+  if (grid.rows[e.row].dataItem?._isAdded !== true && gridKeys.includes(e.getColumn().binding)) {
+    e.cancel = true;
+  }
+};
